838f8f57e58b79d825a65ce66015607eca390923
josephchilin <joseph@josephchilin.com>
2020-05-07 11:11:30 -0400

    fixed edge-case edit error

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,25 +3,20 @@ require_relative '../lib/ingredient'
 require_relative '../lib/recipe'
 require_relative '../lib/recipe_ingredient'
 
-# Recipe (name, instruction)
-# RecipeIngredient (recipe_id, ingredient_id, ingredient_quantity)
-# Ingredient (name)
-
 # TO DO 
 
 # MVP
-# make edit recipe based off add new recipe
-# refactor all tty prompts
-
 # As a user, I want to add a new recipe. (create) DONE
 # As a user, I want to read recipes. (read) DONE
 # As a user, I want to update my recipe. (update)
 # As a user, I want to delete my recipe. (delete) DONE
 
 # EXTRA
-# enable ingredient quantity
+# enable ingredient quantity functionality
 # make search by name
 # make search by ingredient
+
+#=============================================================================================
 
 class CommandLineInterface
 
@@ -33,10 +28,8 @@ class CommandLineInterface
         puts "Welcome to Recipe Finder, your home for delicious food!"
     end
 
-#====NAVIGATION
-
+#====NAVIGATION===============================================================================
     def main_menu
-        prompt = TTY::Prompt.new
     # hash as nav to link to other methods
         # menu_options = {
         #     "View recipes" => view_recipes,
@@ -53,7 +46,7 @@ class CommandLineInterface
         # menu_options[menu_choice]
     
 # refactored menu
-        prompt.select("What would you like to do?") do |menu|
+        @prompt.select("What would you like to do?") do |menu|
             menu.choice "View recipes", -> {view_recipes}
             menu.choice "Add recipe", -> {add_recipe}
             menu.choice "Exit program", -> {quit_program}
@@ -61,8 +54,7 @@ class CommandLineInterface
     end
 
     def nav_menu
-        prompt = TTY::Prompt.new
-        prompt.select("What would you like to do now?") do |menu|
+        @prompt.select("What would you like to do now?") do |menu|
             menu.choice "View recipes", -> {view_recipes}
             menu.choice "Add recipe", -> {add_recipe}
             menu.choice "Edit recipe", -> {edit_recipe}
@@ -71,25 +63,21 @@ class CommandLineInterface
         end
     end
 
-#===RECIPE PAGE
+#===RECIPE PAGE===============================================================================
     def recipe_page(recipe)
-        recipe_name(recipe)
+        puts "Recipe: #{recipe}"
         recipe_ingredients(recipe)
         recipe_instruction(recipe)
-        #name => make recipe_name method
         #ingredients => make display all ingredients method
                 # iterate through Recipe.WHATEVER.ingredients
         #instructions
                     # Recipe.WHATEVER.instruction
     end
 
-#===RECIPE PAGE HELPERS
-    def recipe_instance(recipe_name)
+#===RECIPE PAGE HELPERS=======================================================================
+    #FINDS RECIPE INSTANCE OBJECT BY NAME STRING OF RECIPE
+    def recipe_instance(recipe_name) 
         Recipe.find_by name: recipe_name
-    end
-
-    def recipe_name(name)
-        puts "Recipe: #{name}"
     end
 
     def recipe_ingredients(name)
@@ -109,6 +97,7 @@ class CommandLineInterface
         recipe = recipe_instance(name)
         instruction = recipe.instruction
         puts "Instructions: #{instruction}"
+        instruction
         # => returns instruction for given recipe string name
     end
 
@@ -119,48 +108,53 @@ class CommandLineInterface
     end
 
 
-#===INGREDIENTS HELPER METHODS
+#===INGREDIENTS HELPERS=======================================================================
     def all_ingredients
         Ingredient.all.map do |ingredient|
             ingredient.name 
         end
     end
 
-    def ingredient_instance(ingredient_name)
+    #FINDS INGREDIENT INSTANCE OBJECT BY NAME STRING OF INGREDIENT
+    def ingredient_instance(ingredient_name) 
         Ingredient.find_by name: ingredient_name
     end
 
-#===MENU OPTIONS======
+#===MENU OPTIONS==============================================================================
+# Recipe (name, instruction)
+# RecipeIngredient (recipe_id, ingredient_id, ingredient_quantity)
+# Ingredient (name)
 
-#===VIEW RECIPES======
+# Recipe.create(name: "Fried Rice", instruction: "You make fried rice by frying rice.")
+# RecipeIngredient.create(recipe: r1, ingredient: i3, ingredient_quantity: "4")
+# Ingredient.create(name: "rice")
+
+#===VIEW RECIPES==============================================================================
     def view_recipes
-        prompt = TTY::Prompt.new
-# @recipe_choice carries name string of recipe chosen
-        @recipe_choice = prompt.select("Choose your recipe", all_recipes)
-    #---recipe info-----
+# @recipe_choice holds a string of the chosen recipe's name
+        @recipe_choice = @prompt.select("Choose your recipe", all_recipes)
         recipe_page(@recipe_choice)
+# @current_recipe holds the instance object of currently selected recipe
+        @current_recipe = recipe_instance(@recipe_choice) 
         # binding.pry
         puts "\n"
         nav_menu
     end
 
-# Recipe.create(name: "Fried Rice", instruction: "You make fried rice by frying rice.")
-# Ingredient.create(name: "rice")
-# RecipeIngredient.create(recipe: r1, ingredient: i3, ingredient_quantity: "4")
-    
-#===ADD RECIPE===
+#===ADD RECIPE================================================================================
     def add_recipe
-        prompt = TTY::Prompt.new
-        recipe_name = prompt.ask("What is the name of your recipe?")
+        recipe_name = @prompt.ask("What is the name of your recipe?")
         recipe_name = recipe_name.titleize
         Recipe.create(name: recipe_name, instruction: nil)
+
         add_ingredient
-        recipe_instruction = prompt.ask("How do you cook #{recipe_name}?")
+        
+        recipe_instruction = @prompt.ask("How do you cook #{recipe_name}?")
         Recipe.last.update(name: recipe_name, instruction: recipe_instruction)
 # binding.pry
         puts "Thanks for submitting a recipe for #{recipe_name}!"
         puts "\n"
-    @recipe_choice = recipe_name
+        @recipe_choice = recipe_name #included for edge nav name display case
         nav_menu
     end
 
@@ -176,19 +170,18 @@ class CommandLineInterface
             Recipe.last.ingredients << new_recipe_ingredient
         end
     end
-#===ADD INGREDIENT HELPER==========
+#===ADD INGREDIENT HELPERS=====================================================================
     def add_ingredient
-        prompt = TTY::Prompt.new
-        ingredient = prompt.ask("What is your recipe's first ingredient?")
+        ingredient = @prompt.ask("What is your recipe's first ingredient?")
 
         ingredient_logic(ingredient)
 
     # build more ingredients loop
         loop do
-         more_ingredients = prompt.yes?("Does your recipe have more ingredients?")
+         more_ingredients = @prompt.yes?("Does your recipe have more ingredients?")
         
             if more_ingredients #=> true
-                ingredient = prompt.ask("What is your recipe's next ingredient?")
+                ingredient = @prompt.ask("What is your recipe's next ingredient?")
 
                 ingredient_logic(ingredient)
             else
@@ -197,10 +190,9 @@ class CommandLineInterface
         end
     end
 
-#===EDIT HELPER METHODS====
+#===EDIT RECIPE HELPERS========================================================================
     def edit_menu
-        prompt = TTY::Prompt.new
-        prompt.select("What do you want to edit?") do |menu|
+        @prompt.select("What do you want to edit?") do |menu|
             menu.choice "Edit name", -> {edit_name}
             menu.choice "Edit ingredients", -> {edit_ingredients}
             menu.choice "Edit instructions", -> {edit_instruction}
@@ -211,16 +203,16 @@ class CommandLineInterface
     def edit_name
         new_name = @prompt.ask("What is the new recipe name?")
         new_name = new_name.titleize
-        current_recipe = recipe_instance(@recipe_choice)
-        current_recipe.update(name: new_name)
         puts "Your recipe is now called #{new_name}!"
-        # binding.pry
+                # binding.pry
+        @current_recipe.update(name: new_name)
+
         edit_menu
     end
 
 # add or remove ingredients?
-# if add shovel new ingredient into recipe object
-# if remove show menu of ingredients
+# if add, shovel new ingredient into recipe object
+# if remove, show menu of ingredients
 #       delete ingredient confirmation
 #       delete ingredient
 #       
@@ -230,21 +222,21 @@ class CommandLineInterface
             menu.choice "Delete ingredient(s)", -> {delete_ingredients}
             menu.choice "Back to menu", -> {nav_menu}
         end
-        edit_menu
     end
 
     def add_ingredients
         add_new = @prompt.ask("What ingredient would you like to add to this recipe?")
-        current_recipe = recipe_instance(@recipe_choice)
 
         if all_ingredients.include?(add_new)
             new_recipe_ingredient = ingredient_instance(add_new)
-            current_recipe.ingredients << new_recipe_ingredient
+            @current_recipe.ingredients << new_recipe_ingredient
         else
             new_recipe_ingredient = Ingredient.create(name: add_new)
-            current_recipe.ingredients << new_recipe_ingredient
+            @current_recipe.ingredients << new_recipe_ingredient
         end
+
         add_new = add_new.titleize
+        
         puts "#{add_new} has been added to this recipe!"
 
         edit_menu
@@ -253,14 +245,13 @@ class CommandLineInterface
     def delete_ingredients
         all_recipe_ingredients = recipe_ingredients(@recipe_choice)
    
-        current_recipe = recipe_instance(@recipe_choice)
         delete_choice = @prompt.select("Choose an ingredient to delete", all_recipe_ingredients)
         current_ingredient = ingredient_instance(delete_choice)
         delete_choice = delete_choice.titleize
         
         puts "#{delete_choice} has been deleted."
 
-        current_recipe.ingredients.delete(current_ingredient)
+        @current_recipe.ingredients.delete(current_ingredient)
 
         # binding.pry
         edit_menu
@@ -268,36 +259,39 @@ class CommandLineInterface
 
     def edit_instruction
         new_instruction = @prompt.ask("What are the recipe's new instructions?")
-        current_recipe = recipe_instance(@recipe_choice)
-        current_recipe.update(instruction: new_instruction)
+        @current_recipe.update(instruction: new_instruction)
         puts "Your recipe's instructions have been updated!"
         edit_menu
     end
-#===EDIT RECIPE========
+#===EDIT RECIPE================================================================================
     def edit_recipe
-        prompt = TTY::Prompt.new
-        edit_rec = prompt.yes?("Are you sure you want to edit #{@recipe_choice}?")
+        edit_rec = @prompt.yes?("Are you sure you want to edit #{@recipe_choice}?")
     
         if edit_rec #=> true
             edit_menu
         else
             nav_menu
         end
-
-    # .update
     end
 
-#===DELETE RECIPE======
+    # def edit_more
+    #     edit_rec = @prompt.yes?("Do you have more edits to #{@recipe_choice}?")
+    
+    #     if edit_rec #=> true
+    #         edit_menu
+    #     else
+    #         nav_menu
+    #     end
+    # end
+#===DELETE RECIPE==============================================================================
     def delete_recipe
-        prompt = TTY::Prompt.new
-        current_recipe = recipe_instance(@recipe_choice)
         # binding.pry
-        delete_rec = prompt.yes?("Are you sure you want to delete #{@recipe_choice}?")
+        delete_rec = @prompt.yes?("Are you sure you want to delete #{@recipe_choice}?")
 
         if delete_rec # => true
             puts "Recipe: #{@recipe_choice} has been deleted."
             puts "\n"
-            current_recipe.delete
+            @current_recipe.delete
             main_menu
         else
             puts "Recipe: #{@recipe_choice} has not been deleted."
@@ -306,7 +300,7 @@ class CommandLineInterface
         end
     end
 
-#===QUIT=======
+#===QUIT=======================================================================================
     def quit_program
         puts "Thanks for using me!"
         exit
